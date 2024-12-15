9f4c87feaa0be9517b388636977979f05edce52a
Ruba Dabbas <ruba@al-dabbas.com>
2024-12-16 01:24:29 +0300

    modules

---
+++ 05.modules/01.How_to_import_modules.py
@@ -0,0 +1,28 @@
+# To use a module inside your code:
+import math
+import datetime as dt
+# Examples
+# import pandas as pd
+# import numpy as np
+
+from os import path
+# import matplotlib.pyplot as plt
+
+
+print()
+print(math.pi)
+print()
+
+print(dt.datetime.today())
+print()
+
+print(path.dirname(__file__))
+print()
+
+
+# To install a package into the Python library, that is installed on your pc,
+# use:
+# pip install "package name"
+# This line will be executed in the command prompt terminal
+# And it is better to create a virtual environment and then install the
+# required packages
