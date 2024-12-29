fe31bc9b6f442fd3d710e07a2c773f3bc4448c85
Ruba Dabbas <ruba@al-dabbas.com>
2024-12-30 02:15:07 +0300

    venv create

---
+++ 05.modules/04.venv/venv_create.py
@@ -0,0 +1,27 @@
+# How to create a virtual environment?
+# 1. Installing virtualenv Package:
+# pip install virtualenv
+# or
+# python -m pip install virtualenv
+
+# 2. In your project folder create the virtual environment as follows:
+# python -m venv learnvenv
+
+# "learnvenv" is the virtual environment name.
+
+# 3.Activating the virtual environment:
+# On Windows:
+# learnvenv\Script\actiavte
+
+# On macOS and Linux, the "source" word should be included before the
+# activation line:
+# source learnvenv/bin/activate
+
+# 4. Deactivating the virtual environment, run:
+# deactivate
+
+# To create a venv with a specific Python version, run:
+# virtualenv -p python3.12.2 VirtualPython
+
+# Note: the version you want to use in the venv should
+# be installed on your system.
