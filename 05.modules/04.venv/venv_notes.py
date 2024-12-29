859b3fc92654204a13986ddba6e10256d6a4f7ce
Ruba Dabbas <ruba@al-dabbas.com>
2024-12-30 02:13:32 +0300

    venv revised

---
+++ 05.modules/04.venv/venv_notes.py
@@ -0,0 +1,20 @@
+# Creating virtual environment is a good approach when working on different
+# projects.
+
+# Python Virtual Environment
+# The virtual environment allows you to work in a contained environment
+# without affecting the system Python. Each virtual environment has its
+# directory or folder that contains:
+# 1. Python installation for a particular version.
+# Suppose you are working on a project, where you have to install a package
+# that only works on a specific Python version, you create a virtual
+# environment and install the Python version needed to install this package.
+# Beware any Python version you want to install in a virtual environment,
+# first you should install it in your system.
+
+# 2. Packages or libraries that are needed in your project. This setup allows
+# you to manage dependencies for different projects separately, avoiding
+# conflicts between them.
+# Suppose you are working on two different projects where each one of them
+# uses different packages or each one of them uses the same package but with
+# different versions.
